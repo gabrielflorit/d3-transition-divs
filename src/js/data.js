27fe90f8a47611e2732714d5050b3d002faaba1f
Gabriel Florit <gabrielflorit@gmail.com>
2015-06-18 18:39:15 -0400

    initial commit

---
+++ src/js/data.js
@@ -0,0 +1,59 @@
+var data = [
+	{
+		"name": "gabriel",
+		"before": {
+			"order": 0,
+			"team": "red"
+		},
+		"after": {
+			"order": 4,
+			"team": "orange"
+		}
+	},
+	{
+		"name": "luke",
+		"before": {
+			"order": 1,
+			"team": "blue"
+		},
+		"after": {
+			"order": 3,
+			"team": "green"
+		}
+	},
+	{
+		"name": "russell",
+		"before": {
+			"order": 2,
+			"team": "yellow"
+		},
+		"after": {
+			"order": 2,
+			"team": "red"
+		}
+	},
+	{
+		"name": "james",
+		"before": {
+			"order": 3,
+			"team": "orange"
+		},
+		"after": {
+			"order": 1,
+			"team": "blue"
+		}
+	},
+	{
+		"name": "patrick",
+		"before": {
+			"order": 4,
+			"team": "green"
+		},
+		"after": {
+			"order": 0,
+			"team": "yellow"
+		}
+	},
+];
+
+module.exports = data;
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 window.pym = require('pym.js');
 
+function log(s) {
+	console.log(JSON.stringify(s, null, 4));
+}
+
 (function() {
 	'use strict';
 	// global variables
@@ -10,6 +14,138 @@ window.pym = require('pym.js');
 		if (window.console && console.log) console.log('-- init globe graphic --');
 
 	};
+
+	var data = require('./data.js');
+
+	var d3 = require('d3');
+	var _ = require('lodash');
+	var shuffled;
+
+	var colors = [
+		'red',
+		'blue',
+		'yellow',
+		'green',
+		'orange'
+	];
+
+	log(data);
+
+	document.querySelector('button.shuffle').addEventListener('click', function(e) {
+
+		shuffled = _.shuffle(data);
+		log(shuffled[0]);
+		draw(shuffled);
+
+	});
+
+	function draw(_data) {
+
+		// DATA JOIN
+		var divs = d3.select('.transition-divs').selectAll('div').data(_data);
+
+		// UPDATE
+		divs.attr({
+				'class': function(d) {
+					return 'transition-div ' + d.name;
+				}
+			})
+			.style({
+				'background-color': function(a, b) {
+					// a = datum
+					// b = index of the datum
+					return a.before.team;
+				}
+			});
+
+		// ENTER
+		divs.enter().append('div')
+			.attr({
+				'class': function(d) {
+					return 'transition-div ' + d.name;
+				}
+			})
+			.style({
+				'background-color': function(a, b) {
+					// a = datum
+					// b = index of the datum
+					return a.before.team;
+				}
+			})
+			.html(function(d) {
+				return '<span>' + d.name + '</span>';
+			});
+	}
+
+	draw(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // DATA JOINS
+// 		rects = config.chart.selectAll('rect')
+// 			.data(config.data, d => `${d.name}${d.date}`);
+
+// 		// UPDATE
+// 		rects.transition()
+// 			.duration(config.duration)
+// 			.attr(config.attributes)
+// 			.style(config.style);
+
+// 		// ENTER
+// 		rects.enter().append('rect')
+// 			.attr(config.attributes)
+// 			.style(config.style);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
